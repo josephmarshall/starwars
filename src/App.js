cad48e7209afcb7a6a73292a228eeaee12f0f019
Chris Larsen <c.r.larsen@hotmail.com>
2019-04-24 15:48:51 -0600

    redirect

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import People from "./People";
 import { Container } from "semantic-ui-react";
+import Home from "./Home";
 
 const App = () => (
   <Container style={{ background: "rgba(0,0,0,0.9)" }}>
     <Route exact path="/people" component={People} />
+    <Route exact path="/" component={Home} />
   </Container>
 );
 
